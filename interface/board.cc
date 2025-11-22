f12addbe0e62c62782fbe036f92e0758729d5e6d
Tong Tong <72479071+potatotong@users.noreply.github.com>
2025-11-22 10:35:26 -0500

    Update board.cc

--- interface/board.cc
+++ interface/board.cc
@@ -3,10 +3,11 @@ import ISubject;
 import IObserver;
 import Link;
 import PlayerHeader;
+import <vector>;
 
 export class Board : public ISubject {
     public:
-    int turn_num;
+    int turn_num = 0;
     vector<vector<Link>> board;
     void play_turn(PlayerHeader & ph, int turn_num);
     ~Board();
