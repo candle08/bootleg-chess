7a3c6df563581993b10279981545e8907d690ef9
Tong Tong <72479071+potatotong@users.noreply.github.com>
2025-11-22 11:50:40 -0500

    Create small-swap.cc

---
+++ interface/abilities/small-swap.cc
@@ -0,0 +1,14 @@
+export module SmallSwap;
+import <Board>;
+import Coords;
+import Ability;
+import Player;
+
+using namespace std;
+
+export class SmallSwap : public Ability {
+    string name;
+
+    public:
+    use_power(Player & p1, Player &p2, Board & b, Coords c);
+};
