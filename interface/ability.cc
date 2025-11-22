686349aa590f2d4931c879f1e5856bb5fc088ae6
Tong Tong <72479071+potatotong@users.noreply.github.com>
2025-11-22 11:15:04 -0500

    Update ability.cc

--- interface/ability.cc
+++ interface/ability.cc
@@ -4,9 +4,12 @@ import Player;
 import Board;
 import Coords;
 
+using namespace std;
+
 export class Ability {
     string name;
+
     public:
-    void virtual usePower(Player & p1, Player & p2, Board & b, Coords & c);
+    virtual void usePower(Player &p1, Player &p2, Board &b, Coords &c);
     virtual ~Ability() = default;
 };
