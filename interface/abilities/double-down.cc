c70695b0e14f3d6483166de817e3981123ecf059
Tong Tong <72479071+potatotong@users.noreply.github.com>
2025-11-22 11:50:03 -0500

    Update double-down.cc

--- interface/abilities/double-down.cc
+++ interface/abilities/double-down.cc
@@ -1,1 +1,14 @@
 export module DoubleDown;
+import <Board>;
+import Coords;
+import Ability;
+import Player;
+
+using namespace std;
+
+export class DoubleDown : public Ability {
+    string name;
+
+    public:
+    use_power(Player & p1, Player &p2, Board & b, Coords c);
+};
