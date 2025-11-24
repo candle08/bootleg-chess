021d543253c6d65b38885f7d1bb34136f9f41901
Tong Tong <72479071+potatotong@users.noreply.github.com>
2025-11-24 17:16:57 -0500

    removed racist ctor

--- impl/virus-impl.cc
+++ impl/virus-impl.cc
@@ -4,5 +4,3 @@ module Virus;
 import Coords;
 
 Virus::Virus(int level, Coords coords): level{level}, coords{coords}, type{"virus"} {}
-
-Virus::Virus(int level, int r, int c): level{level}, coords{{r, c}}, type{"virus"} {}
