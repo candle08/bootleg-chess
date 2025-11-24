1ef21c39b283597a100f1cbe640d826c5c6c6320
Tong Tong <72479071+potatotong@users.noreply.github.com>
2025-11-24 17:18:15 -0500

    removed racist ctor

--- impl/data-impl.cc
+++ impl/data-impl.cc
@@ -4,4 +4,3 @@ import Coords;
 
 Data::Data(int level, Coords coords): level{level}, coords{coords}, type{"data"} {}
 
-Data::Data(int level, int r, int c): level{level}, coords{{r, c}}, type{"data"} {}
