d4f1a9dc7aa9aaae4fd9b33ffc0e3135d0e6154f
Tong Tong <72479071+potatotong@users.noreply.github.com>
2025-11-24 16:41:40 -0500

    removed chanty ver of move

--- interface/link.cc
+++ interface/link.cc
@@ -9,13 +9,6 @@ export struct Link {
     Link(int level, Coords coords);
 
     /**
-     * Update the coords of link
-     * @param row The new row of the link
-     * @param col The new col of the link
-     */
-    void move(int row, int col);
-
-    /**
      * Update the coords of the link, but with a Coords parameter
      * @param new_coords The new coords of the link
      */
