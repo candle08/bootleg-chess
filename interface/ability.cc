7542e69f52799c4fc125f9aa071310e152f71f0d
Ryan Zhang <ryan.zhang.623@gmail.com>
2025-11-25 16:42:50 -0500

    added notify and observers

--- interface/ability.cc
+++ interface/ability.cc
@@ -17,7 +17,9 @@ export class Ability {
          * Returns either the empty string on success, or an error message on failure
          * @param board A reference to the playing board
          * @param coords A reference to the coordinates of the ability, or {-1, -1} if not applicable
-         * @param link A pointer to the link targeted in the ability, or nullptr if not applicable
+         * @param link1 A pointer to the first link targeted in the ability, or nullptr if not applicable
+         * @param link2 A pointer to the second link targeted in the ability, or nullptr if not applciable
+         * @param p A pointer to the player using the ability
          */
         virtual string usePower(Board &b, Coords &c, Link* link1, Link* link2, Player * p);
         virtual ~Ability() = 0;
