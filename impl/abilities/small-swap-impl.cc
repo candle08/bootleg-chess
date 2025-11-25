091addcf9b007611f96ec00220e5a2892cacc2fe
chanty <c843zhan@uwaterloo.ca>
2025-11-25 16:12:43 -0500

    fixed

---
+++ impl/abilities/small-swap-impl.cc
@@ -0,0 +1,7 @@
+module SmallSwap;
+
+using namespace std;
+
+string SmallSwap::usePower(Board & b, Coords & b, Link * link1, Link * link2, Player * p) override {
+    
+}
--- impl/abilities/two-sum-impl.cc
+++ impl/abilities/two-sum-impl.cc
@@ -7,7 +7,7 @@ import Player;
 
 using namespace std;
 
-string SmallSum::usePower(Board & b, Coords & b, Link * link1, Link * link2, Player * p) override {
+string TwoSum::usePower(Board & b, Coords & b, Link * link1, Link * link2, Player * p) override {
     if (link1) {
         link1->frozen_on_turn = turn_number;
         return "";
