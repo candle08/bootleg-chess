bddb623844f41484d3117baa565f40bc871de3f2
chanty <c843zhan@uwaterloo.ca>
2025-11-24 21:02:30 -0500

    teehee

--- interface/player.cc
+++ interface/player.cc
@@ -37,7 +37,7 @@ export class Player {
          * Returns a pointer to the corresponding virus or data that the string represents
          * It is assumed that the string is of valid format
          */
-        Link* getLinkPointerFromSymbol(char link);
+        Link* getLinkPointerFromChar(char link);
 
         /**
          * Use and remove the ability represented by the char
